4e463a1df40472fa1b485d14df7c13992cf08f19
MarkoM91 <markomedic91@gmail.com>
2019-03-02 21:13:29 +0100

    commit6

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,1 +1,18 @@
+var span = $('span i.fa-bars');
+var container = $('.container');
+var li = $('.container ul li');
+var body = $('body');
+var a = $('a');
 
+
+span.click(function() {
+
+  span.hide();
+  body.css("background" , "darkblue").css("text-align" , "left")
+  container.show(function() {
+    li.css("display" , "block").css("width" , 400).css("height" , 200).css("font-size" , 36)
+    a.css("text-decoration" , "underline").css("color" , "white")
+
+  });
+  container.height(800)
+});
