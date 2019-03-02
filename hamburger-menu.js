b4d1c18cda5969c54ccd3d27c39d7f1e9c045dd2
MarkoM91 <markomedic91@gmail.com>
2019-03-02 19:21:54 +0100

    commit1

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,16 +1,1 @@
-var icon = $('img');
-var container = $('.container');
-var item = $('#item');
-var img= $('span img');
 
-icon.click(function() {
-  container.slideToggle()
-  icon.hide();
-  img.show();
-});
-
-container.click(function(){
-  container.hide();
-  icon.show();
-
-})
