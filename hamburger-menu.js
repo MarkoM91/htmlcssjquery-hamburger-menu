69bb7306e814be29f76d60d40b021d08c5ffd632
MarkoM91 <markomedic91@gmail.com>
2019-03-03 01:25:25 +0100

    commit8

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -8,7 +8,7 @@ var a = $('a');
 span.click(function() {
 
   span.hide();
-  body.css("background" , "darkblue").css("text-align" , "left")
+  body.css("background" , "rgb(43, 63, 88)").css("text-align" , "left")
   li.removeClass("iscriviti");
   container.show(function() {
     li.css("display" , "block").css("width" , 400).css("height" , 200).css("font-size" , 36)
