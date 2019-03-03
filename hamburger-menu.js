222e2951332985da7db7019d72d98548f997f87e
MarkoM91 <markomedic91@gmail.com>
2019-03-03 19:05:44 +0100

    commit15

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -4,20 +4,22 @@ var container = $('.container');
 var li = $('.container ul li');
 var body = $('body');
 var a = $('a');
+var container2 = $('.hamburger-menù2');
 
 
 
 spanham.click(function() {
 
-  spanham.hide();
-  spancross.css("display" , "block")
+  spanham.hide()
   body.css("background" , "rgb(43, 63, 88)")
-  li.removeClass("iscriviti");
-  container.show(function() {
-    li.css("display" , "block").css("width" , 400).css("height" , 100).css("font-size" , 26).css("position" , "relative").css("right" , "470px").css("padding" , " 80px 0px")
-    a.css("text-decoration" , "underline").css("color" , "white")
+  li.removeClass("iscriviti")
+  container.hide(function() {
 
-  });
+   container2.show()
+   
+  })
+
+
 
 
 });
