5c258264891556ef11c780c0a437ba0e508f9e79
MarkoM91 <markomedic91@gmail.com>
2019-05-31 14:15:54 +0200

    commit21

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -2,17 +2,19 @@ function Exercise(){
   var menu = $('i.fa-bars');
   var hiddennav = $('.hidden-nav');
   var hiddenmenù = $('i.fa-times');
-
+  var iscriviti = $('#iscriviti');
 
   $(document).ready(function() {
 
-   menu.click(function() {
-      hiddennav.addClass("active");
+    menu.click(function() {
 
-   });
-   hiddenmenù.click(function() {
-     hiddennav.removeClass('active');
-   });
-  });
+        hiddennav.addClass("active");
+        iscriviti.removeClass("iscriviti");
+      });
+      hiddenmenù.click(function() {
+        
+        hiddennav.removeClass('active');
+      });
+    });
 }
 Exercise();
